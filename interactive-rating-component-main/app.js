884710bd3d2f31799ca47d622643a2156f405fbc
Emerx27 <91105074+Emerx27@users.noreply.github.com>
2022-05-29 19:44:34 -0500

    Add files via upload

---
+++ interactive-rating-component-main/app.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+    iniciarApp();
+});
+
+function iniciarApp() {
+    buttons();
+    color();
+    submit();
+}
+
+const rate = document.querySelectorAll(".rate");
+const result = document.querySelector(".resultado");
+
+function buttons() {
+    rate.forEach(btn => {
+        btn.addEventListener("click", color);
+
+        btn.addEventListener("click", function(e) {
+            e.preventDefault();
+            btn.classList.add("gris");
+            result.textContent = `You selected ${btn.value} out of 5`;
+            result.classList.add("valor")
+        });
+    });
+}
+
+function color() {
+    rate.forEach(btn => {
+        btn.classList.remove("gris");
+    });
+}
+
+function submit() {
+    const cardThanks = document.querySelector(".card-thanks");
+    const cardRate = document.querySelector(".card");
+    const submit = document.querySelector(".formulario");
+    const body = document.querySelector("body");
+    submit.addEventListener("submit", function(e) {
+        e.preventDefault();
+        if(result.classList.contains("valor")) {
+            cardThanks.classList.remove("none");
+            cardRate.classList.add("none");
+        } else {
+            const alert = document.createElement("P");
+            alert.textContent = "Please choose a number from 1 to 5";
+            alert.classList.add("alerta");
+            body.appendChild(alert);
+            setTimeout(() => {
+                alert.remove();
+            }, 5000);
+        }
+    });
+}
